085221422fd079dfa57a92fe163b2332f7e0108a
jhon jairo pastor <jhon.jairo.tumiri@gmail.com>
2026-04-05 13:59:42 -0300

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   output: "static",
+  
   adapter: node({
     mode: "standalone",
   }),
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [tailwindcss(), sitemap()],
 });
